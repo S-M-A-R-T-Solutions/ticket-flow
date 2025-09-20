1771b3bb25e85a7d92b91ed77cf2aba9b8dbb5b6
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-09-20 05:03:44 -0400

    Add inventory component structure and styles

--- frontend/src/components/Inventory/Inventory.jsx
+++ frontend/src/components/Inventory/Inventory.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
+import { FaCirclePlus } from 'react-icons/fa6';
+
+import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
+import AddClient from '../AddClient';
+
 import './Inventory.css';
 
 export default function Inventory() {
+    const [checker, setChecker] = useState(false);
+
+    const onModalClose = () => {
+        setChecker(true);
+    }
+
     return (
         <section className="inventory-tab">
-            <div className="inventory-header">
+            <div className="tickets-header">
                 <h1>Inventory</h1>
+
+                <div className='add-ticket-btn' style={{ listStyle: "none", display: "flex", flexDirection: "row", gap: "5px" }}>
+                    <FaCirclePlus />
+                    <OpenModalMenuItem
+                        itemText={"Add Part"}
+                        modalComponent={<AddClient setClientsChecker={setChecker} />}
+                        onModalClose={onModalClose}
+                    />
+                </div>
             </div>
 
             <div className="inventory-container">
