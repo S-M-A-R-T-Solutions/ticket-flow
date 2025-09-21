4b5d951ac78e06e5c35cd469098ee593c97af6c0
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-09-21 14:07:15 -0400

    Add pagination and total parts management to Inventory component

--- frontend/src/components/Inventory/Inventory.jsx
+++ frontend/src/components/Inventory/Inventory.jsx
@@ -1,40 +1,92 @@
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 
-import { FaCirclePlus } from 'react-icons/fa6';
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { FaCirclePlus } from "react-icons/fa6";
 
-import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
-import AddClient from '../AddClient';
+import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
 
 import './Inventory.css';
 
+import { getAllPartsThunk, getTotalPartsAmountThunk } from "../../store/parts";
+import PartCard from "../PartCard";
+import AddPart from "../AddPart";
+import { getAllStatusThunk } from "../../store/status";
+
 export default function Inventory() {
-    const [checker, setChecker] = useState(false);
+    const dispatch = useDispatch();
+
+    const allParts = useSelector((state) => state.parts.allParts);
+    const totalParts = useSelector((state) => state.parts.totalPartsAmount);
+
+    const [page, setPage] = useState(1);
+    const [deletePartCkecker, setDeletePartChecker] = useState(false);
+    const [addPartCkecker, setAddPartChecker] = useState(false);
+    const [editPartCkecker, setEditPartChecker] = useState(false);
+
+    const PARTS_PER_PAGE = 5;
+
+    useEffect(() => {
+        dispatch(getTotalPartsAmountThunk());
+        dispatch(getAllPartsThunk(page, PARTS_PER_PAGE));
+        dispatch(getAllStatusThunk());
+        setDeletePartChecker(false);
+        setAddPartChecker(false);
+        setEditPartChecker(false);
+    }, [dispatch, page, addPartCkecker, deletePartCkecker, editPartCkecker]);
+
+    console.log(totalParts, "totalParts");
+
+    const lastPage = Math.ceil(totalParts / PARTS_PER_PAGE);
+
+    // if (!allParts || !totalParts)
+    if (!allParts)
+        return (
+            <section className="inventory-tab">
+                <span className="loader"></span>
+            </section>
+        );
 
     const onModalClose = () => {
-        setChecker(true);
+        setDeletePartChecker(true);
+        setAddPartChecker(true);
+        setEditPartChecker(true);
     }
 
     return (
         <section className="inventory-tab">
-            <div className="tickets-header">
-                <h1>Inventory</h1>
+            <div>
+                <div className="tickets-header">
+                    <h1>Inventory</h1>
 
-                <div className='add-ticket-btn' style={{ listStyle: "none", display: "flex", flexDirection: "row", gap: "5px" }}>
-                    <FaCirclePlus />
-                    <OpenModalMenuItem
-                        itemText={"Add Part"}
-                        modalComponent={<AddClient setClientsChecker={setChecker} />}
-                        onModalClose={onModalClose}
-                    />
+                    <div className='add-ticket-btn' style={{ listStyle: "none", display: "flex", flexDirection: "row", gap: "5px" }}>
+                        <FaCirclePlus />
+                        <OpenModalMenuItem
+                            itemText={"Add Part"}
+                            modalComponent={<AddPart setPartsChecker={setAddPartChecker} />}
+                            onModalClose={onModalClose}
+                            dismisable={false}
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <div className="inventory-container">
+                <div className="inventory-container">
+                    <div>
+                        {allParts.map((part) => (
+                            <PartCard key={part.id} part={part} setEditPartChecker={setEditPartChecker} setDeletePartChecker={setEditPartChecker} />
+                        ))}
+                    </div>
+                </div>
 
-            </div>
-
-            <div className="inventory-footer">
-
+                <div className="tickets-footer">
+                    <button className='prev-btn' style={{ border: "none" }} disabled={page <= 1} onClick={() => setPage(page - 1)}><FaAngleLeft /></button>
+                    <div>
+                        <span >
+                            {page} of {lastPage}
+                        </span>
+                    </div>
+                    <button className='next-btn' style={{ border: "none" }} disabled={page >= lastPage} onClick={() => setPage(page + 1)}><FaAngleRight /></button>
+                </div>
             </div>
         </section>
     );
--- frontend/src/store/parts.js
+++ frontend/src/store/parts.js
@@ -2,6 +2,7 @@ import { csrfFetch } from './csrf';
 
 //Constants
 const GET_ALL_PARTS = 'parts/getAllParts';
+const GET_TOTAL_PARTS_AMOUNT = 'parts/getTotalPartsAmount';
 const GET_PART = 'parts/getPart';
 const ADD_PART = 'parts/addPart';
 const EDIT_PART = 'parts/editPart';
@@ -11,6 +12,11 @@ const DELETE_PART = 'parts/deletePart';
 const getAllParts = (parts) => ({
     type: GET_ALL_PARTS,
     payload: parts
+});
+
+const getTotalPartsAmount = (amount) => ({
+    type: GET_TOTAL_PARTS_AMOUNT,
+    payload: amount
 });
 
 const getPart = (part) => ({
@@ -34,10 +40,16 @@ const deletePart = (part) => ({
 });
 
 //Thunks
-export const getAllPartsThunk = () => async (dispatch) => {
-    const res = await csrfFetch('/api/parts');
+export const getAllPartsThunk = (page, size) => async (dispatch) => {
+    const res = await csrfFetch(`/api/parts?page=${page}&size=${size}`);
     const parts = await res.json();
     dispatch(getAllParts(parts));
+};
+
+export const getTotalPartsAmountThunk = () => async (dispatch) => {
+    const res = await csrfFetch('/api/parts');
+    const amount = await res.json();
+    dispatch(getTotalPartsAmount(amount.length));
 };
 
 export const getPartThunk = (partId) => async (dispatch) => {
@@ -120,13 +132,17 @@ export const deletePartThunk = (partId) => async (dispatch) => {
 //Reducer
 const initialState = {
     allParts: [],
-    part: {}
+    part: {},
+    totalPartsAmount: 0
 };
 
 const partsReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_ALL_PARTS: {
             return { ...state, allParts: action.payload };
+        }
+        case GET_TOTAL_PARTS_AMOUNT: {
+            return { ...state, totalPartsAmount: action.payload };
         }
         case GET_PART: {
             return { ...state, part: action.payload };
