09a6a00a6117cec7799d817f0e7e07dcefe87e60
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-09-24 06:17:50 -0400

    Add SearchBar component and integrate it into TopBar; update styles and layout

--- frontend/src/components/Layout/TopBar/TopBar.tsx
+++ frontend/src/components/Layout/TopBar/TopBar.tsx
@@ -1,10 +1,16 @@
 import "./TopBar.scss";
+import SearchBar from "../SearchBar/SearchBar";
 import ProfileControl from "../ProfileControl";
 
 export default function TopBar() {
     return (
         <div className="top-bar">
+            <div className="search-bar-wrapper">
+                <SearchBar />
+            </div>
+
             <div className="spacer"></div>
+
             <ProfileControl />
         </div>
     );
