9cbc7ee31d712d6d8d26ad23d3376d7d4e0dc1a0
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-09-24 05:43:34 -0400

    Refactor Layout component by removing unused code and simplifying structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,47 +19,6 @@ import Clients from "./components/Clients/Clients";
 import TrackingPage from "./components/TrackingPage/TrackingPage";
 import Inventory from "./components/Inventory";
 
-function _Layout() {
-  const dispatch = useDispatch();
-  // const [isLoaded, setIsLoaded] = useState(false);
-
-  const sessionUser = useSelector(state => state.session.user);
-  const myTickets = useSelector(state => state.tickets.myTickets);
-  const status = useSelector(state => state.status.allStatus);
-
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser())
-    // .then(() => setIsLoaded(true));
-  }, [dispatch]);
-
-  return (
-    <div className="app-div-container">
-      {
-        sessionUser ? (
-          <>
-            <header className="header">
-              <Navigation />
-            </header>
-            <main className='main-zone'>
-              <div className="left-section-mm">
-                <MyWork myTickets={myTickets} status={status} />
-              </div>
-              <div className="right-section-mm">
-                <Outlet />
-              </div>
-            </main>
-            <Footer />
-          </>
-        ) : (
-          <main className='main-zone'>
-            <Outlet />
-          </main>
-        )
-      }
-    </div>
-  )
-}
-
 function Layout() {
   const dispatch = useDispatch();
   // const [isLoaded, setIsLoaded] = useState(false);
