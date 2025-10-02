fe9c23411add5aac6296d577da18c4eefad4cdc1
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-10-02 16:58:59 -0400

    FIXED: Model Misspelling

--- backend/db/seeders/24-demo-scannerImages.js
+++ backend/db/seeders/24-demo-scannerImages.js
@@ -17,7 +17,7 @@ module.exports = {
       },
       {
         scannerId: 3,
-        imageUrl: 'https://mediaserver.goepson.com/ImConvServlet/imconv/ced8277812e66069df3e9354a3e57847ebab5be8/1200Wx1200H?use=banner&hybrisId=B2C&assetDescr=v600_fla-osr-nn_690x460https://5.imimg.com/data5/SELLER/Default/2020/11/ST/SX/ZK/3245790/documents-desktop-scanner-500x500.jpg',
+        imageUrl: 'https://m.media-amazon.com/images/I/61XB+TikfjL.jpg',
         description: 'Scanner 2 Image'
       },
       {
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         scannerId: 4,
-        imageUrl: 'https://i5.walmartimages.com/seo/Brother-Professional-Monochrome-Color-Desktop-Scanner-for-Business-Workgroups-ADS-4300N_a61e7933-589d-45ec-a796-657744c0187d.d2c601464080ccba75133c62b7b74350.jpeg',
+        imageUrl: 'https://m.media-amazon.com/images/I/61XB+TikfjL.jpg',
         description: 'Scanner 4 Image'
       }
     ], options)
