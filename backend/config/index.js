351440716ce359ecd89d81009beb4b05c722532c
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-10-07 09:41:37 -0400

    ADDED: Webhook endpoint to receive call notifications from twilio

--- backend/config/index.js
+++ backend/config/index.js
@@ -5,5 +5,8 @@ module.exports = {
     jwtConfig: {
         secret: process.env.JWT_SECRET,
         expiresIn: process.env.JWT_EXPIRES_IN
+    },
+    twilioConfig: {
+        pbxNumber: process.env.PBX_NUMBER
     }
 };
