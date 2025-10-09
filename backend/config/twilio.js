4d5a256214e4ec2cc261e9c3227e3d9720c46c94
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-10-08 21:52:59 -0400

    REFACTORED: Update Twilio integration to handle call status and create tickets with associated data

--- backend/config/twilio.js
+++ backend/config/twilio.js
@@ -1,4 +1,5 @@
 module.exports = {
     pbxNumber: process.env.TWILIO_PBX_NUMBER,
     answerMessage: process.env.TWILIO_ANSWER_MESSAGE,
+    autoUserId: process.env.TWILIO_AUTO_USER_ID,
 };
--- backend/routes/api/integrations/twilio.js
+++ backend/routes/api/integrations/twilio.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const twilio = require('twilio');
-const config = require('../../../config/twilio');
+const config = require('@/config/twilio');
+const { Ticket, Client, TwilioCall } = require('@/db/models');
+const generateAlphanumericId = require('@utils/randomGenerator');
 
 const urlencodedParser = express.urlencoded({ extended: true });
 
 router.post('/callStart', urlencodedParser, (req, res) => {
-    // const { From, To, CallSid } = req.body;
-    console.log(`callStart: ${JSON.stringify(req.body)}`);
-
     const twiml = new twilio.twiml.VoiceResponse();
     const pbx = config.pbxNumber;
     twiml.say(config.answerMessage);
@@ -18,9 +17,60 @@ router.post('/callStart', urlencodedParser, (req, res) => {
     return res.send(twiml.toString());
 });
 
-router.post('/callStatus', urlencodedParser, (req, res) => {
-    // const { From, To, CallSid, CallStatus } = req.body;
-    console.log(`callStatus: ${JSON.stringify(req.body)}`);
+router.post('/callStatus', urlencodedParser, async (req, res) => {
+    const {
+        Called,
+        CallSid,
+        To,
+        CallStatus,
+        From,
+        CallDuration,
+        AccountSid,
+        ApplicationSid,
+        Caller,
+    } = req.body;
+
+    if (CallStatus !== 'completed') return res.sendStatus(200);
+
+    const clientPhone = From === "" ? Caller : From;
+
+    // FIXME: phoneNumber should be string to accommodate for international numbers
+    const clientByPhone = await Client.findOne({ where: { phoneNumber: clientPhone } });
+
+    if (!clientByPhone) return res.sendStatus(200);
+
+    // TODO: research about twilio call transcriptions and recordings
+
+    try {
+        await sequelize.transaction(async (t) => {
+            const ticket = await Ticket.create({
+                title: "",
+                description: "",
+                checkIn: null,
+                checkOut: null,
+                clientId: clientByPhone.id,
+                statusId: 1,
+                hashedId: generateAlphanumericId(10),
+                createdBy: config.autoUserId,
+            }, { transaction: t });
+
+            await TwilioCall.create({
+                ticketId: ticket.id,
+                called: Called,
+                callSid: CallSid,
+                to: To,
+                callStatus: CallStatus,
+                from: From,
+                callDuration: CallDuration,
+                accountSid: AccountSid,
+                applicationSid: ApplicationSid,
+                caller: Caller,
+            }, { transaction: t });
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
     return res.sendStatus(200);
 });
 
