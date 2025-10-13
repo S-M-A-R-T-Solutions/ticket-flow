1f56ed4b77be5833e76b0a5ece4b484b1becd6db
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-10-13 14:16:30 -0400

    ADDED: File hierarchy of AddLocation Component

---
+++ frontend/src/components/Clients/ClientDetails/AddLocation/AddLocation.jsx
@@ -0,0 +1,8 @@
+export default function AddLocation() {
+    return (
+        <div className="add-location">
+            <h2>Add New Location</h2>
+            {/* Form fields for adding a new location would go here */}
+        </div>
+    );
+}
