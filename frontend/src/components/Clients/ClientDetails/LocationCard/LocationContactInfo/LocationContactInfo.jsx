011240195acfb36eb4e2a10dc6e0a63a7b74f731
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-10-16 12:42:45 -0400

    READY: Add and Delete Contact Information to a Location. Styling included

--- frontend/src/components/Clients/ClientDetails/LocationCard/LocationContactInfo/LocationContactInfo.jsx
+++ frontend/src/components/Clients/ClientDetails/LocationCard/LocationContactInfo/LocationContactInfo.jsx
@@ -1,6 +1,6 @@
 import { FaPhone, FaEnvelope } from "react-icons/fa6";
 import { MdOutlineAddIcCall } from "react-icons/md";
-import { LuMailPlus } from "react-icons/lu";
+import { LuMailPlus, LuMapPin } from "react-icons/lu";
 
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -61,7 +61,7 @@ export default function LocationContactInfo({ locationId }) {
         e.preventDefault();
         if (!email || !emailType) return;
 
-        dispatch(addEmailToALocationThunk(locationId, {email, emailType }));
+        dispatch(addEmailToALocationThunk(locationId, { email, emailType }));
         setEmail("");
         setEmailType("");
         setShowAddEmail(false);
@@ -73,38 +73,44 @@ export default function LocationContactInfo({ locationId }) {
 
     return (
         <div className="location-contact-info-wrapper">
-            <h1>Contact Information</h1>
-
+            <div className="contact-info-header">
+                <h1>Contact Information</h1>
+                <p><LuMapPin /> {location.name}</p>
+            </div>
             <div className="contact-info-details">
                 {/* Phone Section */}
                 <div className="contact-phone-number-list">
-                    <h3>Phone Numbers</h3>
-                    {phones.length > 0 ? (
-                        phones.map((phone) => (
-                            <div key={phone.id} className="phone-number-element">
-                                <div className="phone-section" onClick={() => (window.location.href = `tel:${phone.phoneNumber}`)}>
-                                    <div className="phone-button">
-                                        <FaPhone />
+                    <>
+                        <h3>Phone Numbers</h3>
+                        <div className="phone-number-elements">
+                            {phones.length > 0 ? (
+                                phones.map((phone) => (
+                                    <div key={phone.id} className="phone-number-element">
+                                        <div className="phone-section" onClick={() => (window.location.href = `tel:${phone.phoneNumber}`)}>
+                                            <div className="phone-button">
+                                                <FaPhone />
+                                            </div>
+                                            <div className="phone-number-and-title">
+                                                <span className="phone-type-label">{phone.phoneType}</span>
+                                                <span className="phone-number-label">{formatPhoneNumber(phone.phoneNumber)}</span>
+                                            </div>
+                                        </div>
+                                        <button
+                                            className="delete-button"
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                handleDeletePhoneNumber(phone.id);
+                                            }}
+                                        >
+                                            <IoTrashOutline />
+                                        </button>
                                     </div>
-                                    <div className="phone-number-and-title">
-                                        <span className="phone-type-label">{phone.phoneType}</span>
-                                        <span className="phone-number-label">{formatPhoneNumber(phone.phoneNumber)}</span>
-                                    </div>
-                                </div>
-                                <button
-                                    className="delete-button"
-                                    onClick={(e) => {
-                                        e.stopPropagation();
-                                        handleDeletePhoneNumber(phone.id);
-                                    }}
-                                >
-                                    <IoTrashOutline />
-                                </button>
-                            </div>
-                        ))
-                    ) : (
-                        <p>No phone numbers yet.</p>
-                    )}
+                                ))
+                            ) : (
+                                <p>No phone numbers yet.</p>
+                            )}
+                        </div>
+                    </>
 
                     {!showAddPhone ? (
                         <button className="add-button" onClick={() => setShowAddPhone(true)}>
@@ -112,20 +118,29 @@ export default function LocationContactInfo({ locationId }) {
                         </button>
                     ) : (
                         <form className="add-phone-form" onSubmit={handleAddPhoneNumber}>
-                            <input
-                                type="text"
-                                placeholder="Type (e.g., Mobile, Work)"
-                                value={phoneType}
-                                onChange={(e) => setPhoneType(e.target.value)}
-                            />
-                            <input
-                                type="text"
-                                name="phoneNumber"
-                                placeholder="Phone Number"
-                                maxLength={14}
-                                value={tryFormatPhoneNumber(phoneNumber)}
-                                onChange={(e) => setPhoneNumber(e.target.value)}
-                            />
+                            <div className="form-header"><MdOutlineAddIcCall /> Add Phone</div>
+                            <div className="input-group">
+                                <div className="input-field">
+                                    <label>Phone Type *</label>
+                                    <input
+                                        type="text"
+                                        placeholder="Type (e.g., Mobile, Work)"
+                                        value={phoneType}
+                                        onChange={(e) => setPhoneType(e.target.value)}
+                                    />
+                                </div>
+                                <div className="input-field">
+                                    <label>Phone Number *</label>
+                                    <input
+                                        type="text"
+                                        name="phoneNumber"
+                                        placeholder="Phone Number"
+                                        maxLength={14}
+                                        value={tryFormatPhoneNumber(phoneNumber)}
+                                        onChange={(e) => setPhoneNumber(e.target.value)}
+                                    />
+                                </div>
+                            </div>
                             <div className="form-actions">
                                 <button type="submit">Add</button>
                                 <button type="button" onClick={() => setShowAddPhone(false)}>
@@ -138,33 +153,37 @@ export default function LocationContactInfo({ locationId }) {
 
                 {/* Email Section */}
                 <div className="contact-email-list">
-                    <h3>Email Addresses</h3>
-                    {emails.length > 0 ? (
-                        emails.map((emailObj) => (
-                            <div key={emailObj.id} className="email-address-element">
-                                <div className="email-section" onClick={() => (window.location.href = `mailto:${emailObj.email}`)}>
-                                    <div className="email-button">
-                                        <FaEnvelope />
+                    <>
+                        <h3>Email Addresses</h3>
+                        <div className="email-address-elements">
+                            {emails.length > 0 ? (
+                                emails.map((emailObj) => (
+                                    <div key={emailObj.id} className="email-address-element">
+                                        <div className="email-section" onClick={() => (window.location.href = `mailto:${emailObj.email}`)}>
+                                            <div className="email-button">
+                                                <FaEnvelope />
+                                            </div>
+                                            <div className="email-address-and-title">
+                                                <span className="email-type-label">{emailObj.emailType}</span>
+                                                <span className="email-label">{emailObj.email}</span>
+                                            </div>
+                                        </div>
+                                        <button
+                                            className="delete-button"
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                handleDeleteEmail(emailObj.id);
+                                            }}
+                                        >
+                                            <IoTrashOutline />
+                                        </button>
                                     </div>
-                                    <div className="email-address-and-title">
-                                        <span className="email-type-label">{emailObj.emailType}</span>
-                                        <span className="email-label">{emailObj.email}</span>
-                                    </div>
-                                </div>
-                                <button
-                                    className="delete-button"
-                                    onClick={(e) => {
-                                        e.stopPropagation();
-                                        handleDeleteEmail(emailObj.id);
-                                    }}
-                                >
-                                    <IoTrashOutline />
-                                </button>
-                            </div>
-                        ))
-                    ) : (
-                        <p>No email addresses yet.</p>
-                    )}
+                                ))
+                            ) : (
+                                <p>No email addresses yet.</p>
+                            )}
+                        </div>
+                    </>
 
                     {!showAddEmail ? (
                         <button className="add-button" onClick={() => setShowAddEmail(true)}>
@@ -172,21 +191,32 @@ export default function LocationContactInfo({ locationId }) {
                         </button>
                     ) : (
                         <form className="add-email-form" onSubmit={handleAddEmail}>
-                            <input
-                                type="text"
-                                placeholder="Email Type (e.g., Work, Personal)"
-                                value={emailType}
-                                onChange={(e) => setEmailType(e.target.value)}
-                            />
-                            <input
-                                type="email"
-                                placeholder="Email Address"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
+                            <div className="form-header"><LuMailPlus /> Add Email</div>
+                            <div className="input-group">
+                                <div className="input-field">
+                                    <label>Email Type *</label>
+                                    <input
+                                        type="text"
+                                        placeholder="e.g., Work, Personal"
+                                        value={emailType}
+                                        required={true}
+                                        onChange={(e) => setEmailType(e.target.value)}
+                                    />
+                                </div>
+                                <div className="input-field">
+                                    <label>Email Address *</label>
+                                    <input
+                                        type="email"
+                                        placeholder="Email Address"
+                                        value={email}
+                                        required={true}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+                            </div>
                             <div className="form-actions">
-                                <button type="submit">Add</button>
-                                <button type="button" onClick={() => setShowAddEmail(false)}>
+                                <button type="submit-button-add">Add</button>
+                                <button type="cancel-button-add" onClick={() => setShowAddEmail(false)}>
                                     Cancel
                                 </button>
                             </div>
