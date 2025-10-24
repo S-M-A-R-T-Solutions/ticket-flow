7931f17e8700f9689d610fbe8e076f2fac6c0e1a
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-10-24 10:58:18 -0400

    FIXING MIGRATION

--- backend/db/migrations/49-add-phone-number-to-users.js
+++ backend/db/migrations/49-add-phone-number-to-users.js
@@ -6,17 +6,16 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('users', 'phone', {
+    await queryInterface.addColumn('Users', 'phone', {
       type: Sequelize.STRING(15),
       allowNull: true,
-    }, options);
+    }, { schema: options.schema });
   },
 
   async down(queryInterface, Sequelize) {
     options.tableName = 'Users';
-    await queryInterface.removeColumn('users', 'phone', options);
+    await queryInterface.removeColumn('Users', 'phone', options);
   }
 };
