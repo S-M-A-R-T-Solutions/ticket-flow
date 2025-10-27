a1c6c3abb6d912bf98c455b97acf78de9adaeae5
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-10-27 15:44:58 -0400

    Adding relative time to TicketCard Component

--- frontend/src/components/Statistics/Statistics.jsx
+++ frontend/src/components/Statistics/Statistics.jsx
@@ -6,6 +6,7 @@ export default function Statistics({ myTickets, status }) {
 
     const newTickets = myTickets?.filter(ticket => ticket.statusId === status?.filter(status => status.name === 'Open')[0]?.id).length;
     const inProgress = myTickets?.filter(ticket => ticket.statusId === status?.filter(status => status.name === 'In Progress')[0]?.id).length;
+    const pending = myTickets?.filter(ticket => ticket.statusId === status?.filter(status => status.name === 'Pending')[0]?.id).length;
     const completed = myTickets?.filter(ticket => ticket.statusId === status?.filter(status => status.name === 'Closed')[0]?.id).length;
 
     if (!myTickets || !status) return (
@@ -32,12 +33,19 @@ export default function Statistics({ myTickets, status }) {
                     </div>
                     <FaTicket style={{fontSize: "60px", color:`${status[1]?.color}A1` }}/>
                 </div>
+                <div className="pending-tickets-container">
+                    <div className="left-side">
+                        <h2>Pending</h2>
+                        <span>{pending}</span>
+                    </div>
+                    <FaTicket style={{fontSize: "60px", color:`${status[2]?.color}A1` }}/>
+                </div>
                 <div className="done-tickets-container">
                     <div className="left-side">
                         <h2>Done Tickets</h2>
                         <span>{completed}</span>
                     </div>
-                    <FaTicket style={{fontSize: "60px", color:`${status[2]?.color}A1` }}/>
+                    <FaTicket style={{fontSize: "60px", color:`${status[3]?.color}A1` }}/>
                 </div>
             </div>
         </section>
