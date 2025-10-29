437da0d3494ea762832f3fa0df68e62ae59e6988
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2025-10-29 15:16:11 -0400

    Add tab navigation for statistics insights

--- frontend/src/components/Statistics/Statistics.jsx
+++ frontend/src/components/Statistics/Statistics.jsx
@@ -1,4 +1,5 @@
 import { FaTicket } from "react-icons/fa6";
+import { useState } from "react";
 
 import './Statistics.css';
 
@@ -9,6 +10,13 @@ export default function Statistics({ myTickets, status }) {
     const pending = myTickets?.filter(ticket => ticket.statusId === status?.filter(status => status.name === 'Pending')[0]?.id).length;
     const completed = myTickets?.filter(ticket => ticket.statusId === status?.filter(status => status.name === 'Closed')[0]?.id).length;
 
+    const tabs = [
+        { title: 'Today', key: 'today' },
+        { title: 'All', key: 'all' },
+    ];
+
+    const [selectedTab, setSelectedTab] = useState(tabs[0].key);
+
     if (!myTickets || !status) return (
         <section>
             <span>loading...</span>
@@ -18,36 +26,47 @@ export default function Statistics({ myTickets, status }) {
     return (
         <section className="statistics-tab">
             <h1>Insights</h1>
-            <div className="tickets-insights-container">
-                <div className="new-tickets-container">
-                    <div className="left-side">
-                        <h2>New Tickets</h2>
-                        <span>{newTickets}</span>
+
+            <div className="tabs-container">
+                {tabs.map((tab) => (
+                    <div className={`tab-item ${selectedTab === tab.key ? 'active' : ''}`} key={tab.key} onClick={() => setSelectedTab(tab.key)}>
+                        {tab.title}
                     </div>
-                    <FaTicket style={{fontSize: "60px", color:`${status[0]?.color}A1` }}/>
-                </div>
-                <div className="in-progress-tickets-container">
-                    <div className="left-side">
-                        <h2>In Progress</h2>
-                        <span>{inProgress}</span>
-                    </div>
-                    <FaTicket style={{fontSize: "60px", color:`${status[1]?.color}A1` }}/>
-                </div>
-                <div className="pending-tickets-container">
-                    <div className="left-side">
-                        <h2>Pending</h2>
-                        <span>{pending}</span>
-                    </div>
-                    <FaTicket style={{fontSize: "60px", color:`${status[2]?.color}A1` }}/>
-                </div>
-                <div className="done-tickets-container">
-                    <div className="left-side">
-                        <h2>Done Tickets</h2>
-                        <span>{completed}</span>
-                    </div>
-                    <FaTicket style={{fontSize: "60px", color:`${status[3]?.color}A1` }}/>
-                </div>
+                ))}
             </div>
+
+            {selectedTab === 'all' && (
+                <div className="tickets-insights-container">
+                    <div className="new-tickets-container">
+                        <div className="left-side">
+                            <h2>New Tickets</h2>
+                            <span>{newTickets}</span>
+                        </div>
+                        <FaTicket style={{ fontSize: "60px", color: `${status[0]?.color}A1` }} />
+                    </div>
+                    <div className="in-progress-tickets-container">
+                        <div className="left-side">
+                            <h2>In Progress</h2>
+                            <span>{inProgress}</span>
+                        </div>
+                        <FaTicket style={{ fontSize: "60px", color: `${status[1]?.color}A1` }} />
+                    </div>
+                    <div className="pending-tickets-container">
+                        <div className="left-side">
+                            <h2>Pending</h2>
+                            <span>{pending}</span>
+                        </div>
+                        <FaTicket style={{ fontSize: "60px", color: `${status[2]?.color}A1` }} />
+                    </div>
+                    <div className="done-tickets-container">
+                        <div className="left-side">
+                            <h2>Done Tickets</h2>
+                            <span>{completed}</span>
+                        </div>
+                        <FaTicket style={{ fontSize: "60px", color: `${status[3]?.color}A1` }} />
+                    </div>
+                </div>
+            )}
         </section>
     );
 }
